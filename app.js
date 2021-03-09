90090073e59b186d94314a6c1b76ba6b2d470519
Monika Cyprys <monko.projects@gmail.com>
2021-03-09 20:04:06 +0100

    finish pricing system

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
   const amountPageviews = document.querySelector(".numberOfPageviews");
   const toggle = document.querySelector(".toggle");
   const bill = document.querySelector(".bill");
+  const billForYear = document.querySelector(".billForYear");
+  const yearPrice = document.querySelector(".yearPrice");
   const prices = {
     50: 5,
     100: 10,
@@ -13,15 +15,14 @@
   };
   let yearlyChoice = false;
   let total = 10;
-
+  
   function calculateYearlyPrice() {
     yearlyChoice = true;
-    toggle.classList.toggle("active");
     if (yearlyChoice) {
       const allYearPrice = total * 12;
       const discount = allYearPrice / 4;
-      //   billForYear.textContent = `$ ${allYearPrice - discount}.00`;
-      bill.textContent = `$ ${(allYearPrice - discount) / 12}0`;
+      billForYear.textContent = (allYearPrice - discount).toFixed(2);
+      bill.textContent = ((allYearPrice - discount) / 12).toFixed(2);
     }
   }
 
@@ -29,15 +30,25 @@
     amountPageviews.textContent = this.value;
     for (let price in prices) {
       if (this.value == price) {
-        if (!yearlyChoice) {
-          bill.textContent = `$ ${prices[price]}.00`;
-        }
         total = prices[price];
+        if (!yearlyChoice) {
+          bill.textContent = prices[price].toFixed(2);
+        } else calculateYearlyPrice();
       }
     }
-
-    console.log(total);
+  }
+  function toggleIsChecked() {
+    if (toggle.classList.contains("active")) {
+      yearPrice.classList.remove("active");
+      toggle.classList.remove("active");
+      yearlyChoice = false;
+      bill.textContent = total.toFixed(2);
+    } else {
+      yearPrice.classList.add("active");
+      toggle.classList.add("active");
+      calculateYearlyPrice();
+    }
   }
   thumbValue.addEventListener("change", calculateBill);
-  toggle.addEventListener("click", calculateYearlyPrice);
+  toggle.addEventListener("click", toggleIsChecked);
 })();
